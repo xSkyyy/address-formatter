74613de0e5b70b6dffb1ee94c3cc57f62f7c86c4
matty <mc360masteryt@gmail.com>
2021-04-23 22:05:32 +0100

    FUCKING TYPOS MAKE ME ANGY

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 number=(input("What is your house number? "))
 street=(input("what is your street name?  "))
 town=(input("what town do you live in? "))
-print("Your address is ", number, street, ",", town)wow
+print("Your address is ", number, street, ",", town)
